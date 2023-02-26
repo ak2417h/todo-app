9a574c9841fa0d6f347776dac9ac9346f6fc5ea4
Andrew Kim <andrewk2417@gmail.com>
2023-02-26 13:27:55 -0600

    setup auth and firestore

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -9,8 +11,10 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
+  String _name = "";
   String _email = "";
   String _pw = "";
+  bool hp = false;
 
   Future signupmethod() async {
     try {
@@ -22,13 +26,16 @@ class _signupState extends State<signup> {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(e.code)));
     }
+    final db = FirebaseFirestore.instance
+        .collection("user")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .set({"name": _name});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        alignment: Alignment.center,
         decoration: BoxDecoration(
             gradient: LinearGradient(
           begin: Alignment.topCenter,
@@ -40,49 +47,108 @@ class _signupState extends State<signup> {
           ],
         )),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.33,
+              height: MediaQuery.of(context).size.height * 0.25,
             ),
             Container(
-              width: MediaQuery.of(context).size.width * 0.75,
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 35),
+              child: Text(
+                "Create\nAccount",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.15,
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.8,
               child: TextField(
-                decoration: InputDecoration(hintText: "Email"),
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  prefixIcon: Icon(
+                    Icons.person,
+                  ),
+                ),
+                onChanged: (value) => setState(() {
+                  _name = value;
+                }),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: Icon(
+                    Icons.email,
+                  ),
+                ),
                 onChanged: (value) => setState(() {
                   _email = value;
                 }),
               ),
             ),
             Container(
-              width: MediaQuery.of(context).size.width * 0.75,
+              margin: EdgeInsets.only(bottom: 35),
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.8,
               child: TextField(
-                decoration: InputDecoration(hintText: "Password"),
+                obscureText: hp,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      hp ? Icons.remove_red_eye : Icons.remove_red_eye_outlined,
+                    ),
+                    onPressed: () => setState(() {
+                      hp = !hp;
+                    }),
+                  ),
+                ),
                 onChanged: (value) => setState(() {
                   _pw = value;
                 }),
               ),
             ),
-            Container(
-                child: TextButton(
-                    onPressed: () => signupmethod(),
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.black),
-                    ))),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
+              height: 40,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                ),
+                onPressed: () => signupmethod(),
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
-            Container(
-              child: Text("HI"),
-            )
           ],
         ),
       ),
+      /*
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () => null,
         child: Icon(Icons.abc),
       ),
+      */
     );
   }
 }
