4b651062092b09af2b243c2422159de284c78a07
Andrew Kim <andrewk2417@gmail.com>
2023-02-26 21:14:27 -0600

    .

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -8,10 +10,29 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  String a = "";
+
+  Future<void> func() async {
+    final db = FirebaseFirestore.instance
+        .collection("user")
+        .doc(FirebaseAuth.instance.currentUser?.uid);
+    a = await db.get().then((value) => value.data()!["name"]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("HELLO WORLD")),
+      body: Column(children: [
+        Text(
+          "Welcome",
+        ),
+        TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushNamed(context, "signup");
+            },
+            child: Text("CLICK"))
+      ]),
     );
   }
 }
