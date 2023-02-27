fbe82ee1366fbd4dd4e2d7dd10febedb7d398ccc
Andrew Kim <andrewk2417@gmail.com>
2023-02-27 01:50:42 -0600

    major changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,17 +12,44 @@ class homepage extends StatefulWidget {
 
 class _homepageState extends State<homepage> {
   @override
+  int ci = 0;
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(children: [
+    List<Widget> wl = [
         name(),
+        Text("NOTHING"),
         TextButton(
             onPressed: () async {
               await FirebaseAuth.instance.signOut();
               Navigator.pushNamed(context, "login");
             },
             child: Text("Logout"))
-      ]),
+
+    ];
+    return Scaffold(
+      body: Container(
+        child: wl.elementAt(ci),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: ci,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Start",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.man),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          )
+        ],
+        onTap: (value) => setState(() {
+          ci = value;
+        }),
+      ),
     );
   }
 }
