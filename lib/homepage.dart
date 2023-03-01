16c731d9630b3ba881ffeb3bf5e879a4784afad6
Andrew Kim <andrewk2417@gmail.com>
2023-03-01 01:24:42 -0600

    .

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'hpClass.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -16,15 +20,34 @@ class _homepageState extends State<homepage> {
 
   Widget build(BuildContext context) {
     List<Widget> wl = [
-        name(),
-        Text("NOTHING"),
-        TextButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushNamed(context, "login");
-            },
-            child: Text("Logout"))
-
+      name(),
+      Text("NOTHING"),
+      Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushNamed(context, "login");
+              },
+              child: Text("Logout"),
+            ),
+            TextButton(
+              onPressed: () async {
+                final db = FirebaseFirestore.instance
+                    .collection("user")
+                    .doc(FirebaseAuth.instance.currentUser?.uid)
+                    .delete()
+                    .then((value) async {
+                  await FirebaseAuth.instance.currentUser?.delete();
+                  Navigator.pushNamed(context, "signup");
+                });
+              },
+              child: Text("Delete Account"),
+            )
+          ],
+        ),
+      )
     ];
     return Scaffold(
       body: Container(
